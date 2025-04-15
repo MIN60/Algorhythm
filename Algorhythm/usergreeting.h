2330858d536d9877e7915f2add6369263050048e
abab2349 <abab23490@gmail.com>
2025-04-15 14:28:38 +0900

    feature/#20

--- Algorhythm/usergreeting.h
+++ Algorhythm/usergreeting.h
@@ -1,10 +1,13 @@
 #ifndef USERGREETING_H
 #define USERGREETING_H
-
 #include <QWidget>
 #include <QLabel>
 #include <QVBoxLayout>
+#include <QDialog>
+#include <QLineEdit>
+#include <QPushButton>
 #include "usertiercard.h"
+#include "networkmanager.h"
 
 class UserGreeting : public QWidget {
     Q_OBJECT
@@ -13,11 +16,30 @@ public:
     void setUserName(const QString& name);
     void setTier(const QString& tierName, int solvedCount);
 
+protected:
+    bool eventFilter(QObject *watched, QEvent *event) override;
+
+private slots:
+    void showLoginDialog();
+    void attemptLogin();
+    void processUserData(const QJsonObject &userData);
+    void handleNetworkError(const QString &errorMsg);
+
 private:
     QLabel* greetingLabel;
     QLabel* nameLabel;
     UserTierCard* tierCard;
+    QWidget* clickableArea;
+    NetworkManager* networkManager;
+    bool loggedIn;
+    QString currentUsername;
+
+    // 로그인 다이얼로그
+    QDialog* loginDialog;
+    QLineEdit* usernameInput;
+    QPushButton* loginButton;
+    QLabel* statusLabel;
+
+    void logoutUser();
 };
-
-
 #endif // USERGREETING_H
